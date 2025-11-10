030b2a7802e63741b56e6d95052e29fd18b30f4a
Angelenamorado777 <albertoenamorado123@gmail.com>
2025-11-09 23:56:28 -0600

    Commit

--- Abstraccion/Program.cs
+++ Abstraccion/Program.cs
@@ -6,25 +6,25 @@
         {
             Nombre = "Fernando Perez",
             ID = 1602,
-            SalarioBase = 5000,
-            BonoAnual = 12000
+            SalarioBase = 12000,
+            BonoAnual = 20000
         };
 
         Desarrollador dev = new Desarrollador
         {
             Nombre = "Angel Enamorado",
             ID = 1603,
-            SalarioBase = 4000,
-            LenguajePrincipal = "C#"
+            SalarioBase = 10000,
+            LenguajePrincipal = "Python"
         };
 
         Contador contador = new Contador
         {
             Nombre = "Oscar Perdomo",
             ID = 1604,
-            SalarioBase = 3500,
-            HorasExtras = 20,
-            PagoPorHoraExtra = 50
+            SalarioBase = 9000,
+            HorasExtras = 30,
+            PagoPorHoraExtra = 60
         };
     
         gerente.MostrarInformacion();
